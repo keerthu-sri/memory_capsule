7e459a19086eee2679a307a62f84e4f5462de8e1
keerthu-sri <keerthanaworkspace13@gmail.com>
2026-05-02 18:20:50 +0530

    updated finalcors

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,9 @@ const allowedOrigins = [
   "https://memory-capsule-murex.vercel.app"
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: "*"
+}));
 
 app.use(express.json());
 
